7e1ec2c30993a50af9ea5098d89a318de4cafa63
meddlin <drushing.dev@gmail.com>
2018-12-12 22:37:24 -0800

    Routing pulled in via react-router. New Home, Header components, and existing functionality moved to their own "pages".

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
-import Hello from './Hello.jsx';
-import Info from './Info.jsx';
+import { Router, Route } from 'react-router-dom';
+import history from './history';
+
+import Header from './Header';
+import Home from './Home';
 import FileUpload from './FileUpload';
 import ScriptSelector from './ScriptSelector';
 
 const App = () => (
-  <div>
-    <FileUpload />
-    <ScriptSelector />
+  <Router history={history}>
+  <div id="app">
+  	<Header />
+  	<Route path="/" exact={true} component={Home} />
+  	<Route path="/upload" component={FileUpload} />
+  	<Route path="/scripts" component={ScriptSelector} />
   </div>
+  </Router>
 );
 
 export default App;
