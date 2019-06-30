d0665dfbfe92ef778ae928dab22f01c8dfa8add3
meddlin <drushing.dev@gmail.com>
2019-06-29 23:21:31 -0700

    Pulled example Fabric UI panel into sidebar portion of Header. Also initialized icons from Fabric UI in the project.

--- app/client/main.jsx
+++ app/client/main.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 import App from '/imports/ui/App';
+import { initializeIcons } from '@uifabric/icons';
 
 Meteor.startup(() => {
-  render(<App />, document.getElementById('react-target'));
+	initializeIcons();
+  	render(<App />, document.getElementById('react-target'));
 });
