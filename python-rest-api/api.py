0a8b0db4af8c65609db2f4923b1ee76793ee6d34
meddlin <drushing.dev@gmail.com>
2019-10-20 11:38:27 -0700

    Adding nmap to python rest api as proof of concept - Shows it's possible, with very little code, to make the same call you could in a terminal

--- python-rest-api/api.py
+++ python-rest-api/api.py
@@ -1,10 +1,20 @@
 import flask
+import pprint
+from subprocess import Popen, PIPE
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
+printer = pprint.PrettyPrinter(indent=4)
 
 @app.route('/', methods=['GET'])
 def home():
 	return "<h1>Python API worked</h1>"
+
+@app.route('/nmap', methods=['GET'])
+def nmap():
+	p = Popen("nmap", stdout = PIPE)
+	result = p.communicate()[0]
+	printer.pprint(result)
+	return result
 
 app.run()
