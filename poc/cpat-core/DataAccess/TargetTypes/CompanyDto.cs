e16170922bc5435b9a79cf2ffc45ec989e3721e9
meddlin <drushing.dev@gmail.com>
2020-02-19 02:04:28 -0600

    Company target type controller -> DB connections made. AND .csproj updated to produce XML docs.
    Basic CRUD operations in the controller.
    CompanyQuery updated to support the operations provided in the controller.
    CompanyDto has translation methods.
    Company has a collection translation method.

--- poc/cpat-core/DataAccess/TargetTypes/CompanyDto.cs
+++ poc/cpat-core/DataAccess/TargetTypes/CompanyDto.cs
@@ -1,5 +1,7 @@
+using cpat_core.Models;
 using NPoco;
 using System;
+using System.Collections.Generic;
 
 namespace cpat_core.DataAccess.TargetTypes
 {
@@ -19,5 +21,38 @@ namespace cpat_core.DataAccess.TargetTypes
         [Column("datecreated")] public DateTime DateCreated { get; set; }
         [Column("updatedat")] public DateTime UpdatedAt { get; set; }
         [Column("lastmodifiedbyuserid")] public Guid LastModifiedByUserId { get; set; }
+
+        /// <summary>
+        /// Converts a <c>Company</c> object to a <c>CompanyDto</c> object.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static CompanyDto Translate(Company data)
+        {
+            return new CompanyDto()
+            {
+                Id = Guid.NewGuid(),
+                Name = data.Name,
+                // DocumentRelationJson = data.DocumentRelations,
+                DateCreated = data.DateCreated != null ? data.DateCreated : DateTime.Now,
+                UpdatedAt = data.UpdatedAt != null ? data.UpdatedAt : DateTime.Now
+            };
+        }
+
+        /// <summary>
+        /// Converts a collection of <c>Company</c> to a collection of <c>CompanyDto</c>.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static IEnumerable<CompanyDto> Translate(List<Company> data)
+        {
+            var dtoList = new List<CompanyDto>();
+            data.ForEach(d =>
+            {
+                dtoList.Add(CompanyDto.Translate(d));
+            });
+
+            return dtoList;
+        }
     }
 }
