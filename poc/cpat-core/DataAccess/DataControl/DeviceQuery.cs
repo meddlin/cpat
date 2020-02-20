e0b514c5da5a3e61c1552b0256d6d028b7aa1814
meddlin <drushing.dev@gmail.com>
2020-02-20 00:39:36 -0600

    "Data stack" for the Device target type updated.
    Controller, queries, and translation methods.

--- poc/cpat-core/DataAccess/DataControl/DeviceQuery.cs
+++ poc/cpat-core/DataAccess/DataControl/DeviceQuery.cs
@@ -17,6 +17,11 @@ namespace cpat_core.DataAccess.DataControl
             dbAccess = new DataAccess.Database();
         }
 
+        /// <summary>
+        /// Retrieve a single <c>Device</c>.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public Device GetSingle(Guid id)
         {
             Device device;
@@ -32,6 +37,99 @@ namespace cpat_core.DataAccess.DataControl
             }
 
             return device;
+        }
+
+        public IEnumerable<Device> GetCollection(IEnumerable<Guid> ids)
+        {
+            //using (var conn = new NpgsqlConnection(dbAccess.connectionString))
+            //{
+            //    using (var db = new NPoco.Database(conn))
+            //    {
+            //        db.Connection.Open();
+
+            //        db.
+
+            //        db.Connection.Close();
+            //    }
+            //}
+
+            return new List<Device>();
+        }
+
+        /// <summary>
+        /// Insert a single <c>Device</c> object.
+        /// </summary>
+        /// <param name="device"></param>
+        public void Insert(Device device)
+        {
+            using (var conn = new NpgsqlConnection(dbAccess.connectionString))
+            {
+                using (var db = new NPoco.Database(conn))
+                {
+                    db.Connection.Open();
+                    db.Save<DeviceDto>(DeviceDto.Translate(device));
+                    db.Connection.Close();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Batch inserts a collection of <c>Device</c> objects.
+        /// </summary>
+        /// <param name="devices"></param>
+        public void Insert(IEnumerable<Device> devices)
+        {
+            using (var conn = new NpgsqlConnection(dbAccess.connectionString))
+            {
+                using (var db = new NPoco.Database(conn))
+                {
+                    db.Connection.Open();
+                    db.InsertBatch<DeviceDto>(DeviceDto.Translate(devices.ToList()));
+                    db.Connection.Close();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Update a <c>DeviceDto</c> object.
+        /// </summary>
+        /// <param name="device">A <c>Device</c>. This will be translated to a <c>DeviceDto</c> before the update operation.</param>
+        /// <returns><c>int</c> denoting the success value of the operation.</returns>
+        public int Update(Device device)
+        {
+            int res;
+            using (var conn = new NpgsqlConnection(dbAccess.connectionString))
+            {
+                using (var db = new NPoco.Database(conn))
+                {
+                    db.Connection.Open();
+                    res = db.Update(DeviceDto.Translate(device));
+                    db.Connection.Close();
+                }
+            }
+
+            return res;
+        }
+
+        /// <summary>
+        /// Deletes a <c>DeviceDto</c> object.
+        /// </summary>
+        /// <param name="device">A <c>Device</c>. This will be translated to a <c>DeviceDto</c> before the update operation.</param>
+        /// <returns><c>int</c> denoting the success value of the operation.</returns>
+        public int Remove(Device device)
+        {
+            int res;
+            using (var conn = new NpgsqlConnection(dbAccess.connectionString))
+            {
+                using (var db = new NPoco.Database(conn))
+                {
+                    db.Connection.Open();
+                    res = db.Delete(DeviceDto.Translate(device));
+                    db.Connection.Close();
+                }
+            }
+
+            return res;
         }
     }
 }
--- poc/cpat-core/DataAccess/TargetTypes/DeviceDto.cs
+++ poc/cpat-core/DataAccess/TargetTypes/DeviceDto.cs
@@ -1,5 +1,7 @@
-﻿using NPoco;
+﻿using cpat_core.Models;
+using NPoco;
 using System;
+using System.Collections.Generic;
 
 namespace cpat_core.DataAccess.TargetTypes
 {
@@ -20,5 +22,39 @@ namespace cpat_core.DataAccess.TargetTypes
         [Column("datecreated")] public DateTime DateCreated { get; set; }
         [Column("updatedat")] public DateTime UpdatedAt { get; set; }
         [Column("lastmodifiedbyuserid")] public Guid LastModifiedByUserId { get; set; }
+
+        /// <summary>
+        /// Converts a <c>Device</c> object to a <c>DeviceDto</c> object.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static DeviceDto Translate(Device data)
+        {
+            return new DeviceDto()
+            {
+                Id = Guid.NewGuid(),
+                Name = data.Name,
+                // Organizations = data.Organizations,
+                // DocumentRelationJson = data.Relations,
+                DateCreated = data.DateCreated != null ? data.DateCreated : DateTime.Now,
+                UpdatedAt = data.UpdatedAt != null ? data.UpdatedAt : DateTime.Now
+            };
+        }
+
+        /// <summary>
+        /// Converts a collection of <c>Device</c> to a collection of <c>DeviceDto</c>.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static IEnumerable<DeviceDto> Translate(List<Device> data)
+        {
+            var dtoList = new List<DeviceDto>();
+            data.ForEach(d =>
+            {
+                dtoList.Add(DeviceDto.Translate(d));
+            });
+
+            return dtoList;
+        }
     }
 }
