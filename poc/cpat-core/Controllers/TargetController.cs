6f26069e240daf7680e4abb5c46975675a82a379
meddlin <drushing.dev@gmail.com>
2020-02-27 22:12:19 -0600

    Target types updated.
    Target controller, queries, data transfer, and model are updated.

--- poc/cpat-core/Controllers/TargetController.cs
+++ poc/cpat-core/Controllers/TargetController.cs
@@ -1,4 +1,5 @@
-﻿using cpat_core.Models;
+﻿using cpat_core.DataAccess.DataControl;
+using cpat_core.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
@@ -28,6 +29,52 @@ namespace cpat_core.Controllers
                 new Target() { Name = "Alice" },
                 new Target() { Name = "Bob" }
             };
+        }
+
+        /// <summary>
+        /// Insert a single <c>Target</c> record.
+        /// </summary>
+        /// <param name="data"></param>
+        [HttpPost]
+        public void Insert([FromBody] Target data)
+        {
+            var query = new TargetQuery();
+            query.Insert(data);
+        }
+
+        /// <summary>
+        /// Insert a collection of <c>Target</c> records.
+        /// </summary>
+        /// <param name="data"></param>
+        [HttpPost]
+        public void InsertList([FromBody] List<Target> data)
+        {
+            var query = new TargetQuery();
+            query.Insert(data);
+        }
+
+        /// <summary>
+        /// Update a <c>Target</c> record.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public int Update([FromBody] Target data)
+        {
+            var query = new TargetQuery();
+            return query.Update(data);
+        }
+
+        /// <summary>
+        /// Remove a <c>Target</c> record.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public int Remove([FromBody] Target data)
+        {
+            var query = new TargetQuery();
+            return query.Remove(data);
         }
     }
 }
--- poc/cpat-core/DataAccess/DataControl/TargetQuery.cs
+++ poc/cpat-core/DataAccess/DataControl/TargetQuery.cs
@@ -38,5 +38,98 @@ namespace cpat_core.DataAccess.DataControl
 
             return target;
         }
+
+        public IEnumerable<Target> GetCollection(IEnumerable<Guid> ids)
+        {
+            //using (var conn = new NpgsqlConnection(dbAccess.connectionString))
+            //{
+            //    using (var db = new NPoco.Database(conn))
+            //    {
+            //        db.Connection.Open();
+
+            //        db.
+
+            //        db.Connection.Close();
+            //    }
+            //}
+
+            return new List<Target>();
+        }
+
+        /// <summary>
+        /// Insert a single <c>Target</c> object.
+        /// </summary>
+        /// <param name="device"></param>
+        public void Insert(Target device)
+        {
+            using (var conn = new NpgsqlConnection(dbAccess.connectionString))
+            {
+                using (var db = new NPoco.Database(conn))
+                {
+                    db.Connection.Open();
+                    db.Save<TargetDto>(TargetDto.Translate(device));
+                    db.Connection.Close();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Batch inserts a collection of <c>Target</c> objects.
+        /// </summary>
+        /// <param name="devices"></param>
+        public void Insert(IEnumerable<Target> devices)
+        {
+            using (var conn = new NpgsqlConnection(dbAccess.connectionString))
+            {
+                using (var db = new NPoco.Database(conn))
+                {
+                    db.Connection.Open();
+                    db.InsertBatch<TargetDto>(TargetDto.Translate(devices.ToList()));
+                    db.Connection.Close();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Update a <c>TargetDto</c> object.
+        /// </summary>
+        /// <param name="device">A <c>Target</c>. This will be translated to a <c>TargetDto</c> before the update operation.</param>
+        /// <returns><c>int</c> denoting the success value of the operation.</returns>
+        public int Update(Target device)
+        {
+            int res;
+            using (var conn = new NpgsqlConnection(dbAccess.connectionString))
+            {
+                using (var db = new NPoco.Database(conn))
+                {
+                    db.Connection.Open();
+                    res = db.Update(TargetDto.Translate(device));
+                    db.Connection.Close();
+                }
+            }
+
+            return res;
+        }
+
+        /// <summary>
+        /// Deletes a <c>TargetDto</c> object.
+        /// </summary>
+        /// <param name="device">A <c>Target</c>. This will be translated to a <c>TargetDto</c> before the update operation.</param>
+        /// <returns><c>int</c> denoting the success value of the operation.</returns>
+        public int Remove(Target device)
+        {
+            int res;
+            using (var conn = new NpgsqlConnection(dbAccess.connectionString))
+            {
+                using (var db = new NPoco.Database(conn))
+                {
+                    db.Connection.Open();
+                    res = db.Delete(TargetDto.Translate(device));
+                    db.Connection.Close();
+                }
+            }
+
+            return res;
+        }
     }
 }
