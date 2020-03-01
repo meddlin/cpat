95fd1aed576e443deaff81baa6e3b3f236012fc8
meddlin <drushing.dev@gmail.com>
2020-03-01 16:33:04 -0600

    Fixed UX consistency in slide-out menu and NavigationBar
    - Now when clicking any link in the slide-out menu it automatically retracts
    - The app header title is a link back to the root

--- poc/cpat-client/src/components/NavigationBar.js
+++ poc/cpat-client/src/components/NavigationBar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Button, Pane, SideSheet, Position, Heading, Icon } from 'evergreen-ui';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import styled from 'styled-components';
 
 const StyledBar = styled.div`
@@ -11,11 +11,13 @@ const StyledBar = styled.div`
     h2 {
         margin: 0;
         margin-left: 20px;
+        cursor: pointer;
     }
 `;
 
 const NavigationBar = () => {
     const [isShown, setIsShown] = useState(false);
+    let history = useHistory();
 
     return (
         <StyledBar style={{ display: 'flex' }}>
@@ -29,22 +31,22 @@ const NavigationBar = () => {
                 <Pane flex="1" background="tint1" padding={16}>
                     <Heading size={500}>View current</Heading>
                     <ul>
-                        <li><Link to="/companies">Companies</Link></li>
-                        <li><Link to="/devices">Devices</Link></li>
-                        <li><Link to="/locations">Locations</Link></li>
-                        <li><Link to="/people">People</Link></li>
-                        <li><Link to="/targets">Targets</Link></li>
+                        <li><Link to="/companies" onClick={() => setIsShown(false)}>Companies</Link></li>
+                        <li><Link to="/devices" onClick={() => setIsShown(false)}>Devices</Link></li>
+                        <li><Link to="/locations" onClick={() => setIsShown(false)}>Locations</Link></li>
+                        <li><Link to="/people" onClick={() => setIsShown(false)}>People</Link></li>
+                        <li><Link to="/targets" onClick={() => setIsShown(false)}>Targets</Link></li>
                     </ul>
                 </Pane>
                 <hr />
                 <Pane flex="1" background="tint1" padding={16}>
                     <Heading size={500}>Add New sub-targets</Heading>
                     <ul>
-                        <li><Link to="/company/create">Company</Link></li>
-                        <li><Link to="/device/create">Device</Link></li>
-                        <li><Link to="/location/create">Location</Link></li>
-                        <li><Link to="/person/create">Person</Link></li>
-                        <li><Link to="/target/create">Target</Link></li>
+                        <li><Link to="/company/create" onClick={() => setIsShown(false)}>Company</Link></li>
+                        <li><Link to="/device/create" onClick={() => setIsShown(false)}>Device</Link></li>
+                        <li><Link to="/location/create" onClick={() => setIsShown(false)}>Location</Link></li>
+                        <li><Link to="/person/create" onClick={() => setIsShown(false)}>Person</Link></li>
+                        <li><Link to="/target/create" onClick={() => setIsShown(false)}>Target</Link></li>
                     </ul>
                 </Pane>
             </SideSheet>
@@ -52,7 +54,10 @@ const NavigationBar = () => {
                 <Icon icon="list" color="muted" />
             </Button>
 
-            <Heading size={600} marginTop="default">CPAT</Heading>
+            <Heading 
+                size={600} 
+                marginTop="default" 
+                onClick={() => history.push('/')}>CPAT</Heading>
         </StyledBar>
     );
 };
