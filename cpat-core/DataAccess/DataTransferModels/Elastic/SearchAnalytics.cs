aa660ae67934d921c342d8287e20c91472861125
meddlin <drushing.dev@gmail.com>
2020-07-25 02:03:42 -0500

    Getting some basic linkages in place for Elastic connections to cpat-core
    - Two new controllers, one for search queries, the other for analytics queries
    - Document model types for the data transfer objects moving in/out of Elastic
    - Package references for Elastic

---
+++ cpat-core/DataAccess/DataTransferModels/Elastic/SearchAnalytics.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cpat_core.DataAccess.DataTransferModels.Elastic
+{
+    public class SearchAnalytics
+    {
+    }
+}
---
+++ cpat-core/DataAccess/DataTransferModels/Elastic/TargetDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cpat_core.DataAccess.DataTransferModels.Elastic
+{
+    public class TargetDto
+    {
+        public string Name { get; set; }
+    }
+}
