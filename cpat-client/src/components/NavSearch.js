1c85ef28752417aa747d8e24f4fe8ee39720a8eb
meddlin <drushing.dev@gmail.com>
2020-07-25 22:56:10 -0500

    Displaying search results
    - Results are displayed in a panel that slides out from the right. This allows searching and viewing results from anywhere within the app.

--- cpat-client/src/components/NavSearch.js
+++ cpat-client/src/components/NavSearch.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import { SearchInput } from 'evergreen-ui';
+import React, { useState } from 'react';
+import { Pane, SideSheet, Heading, Paragraph, Card, SearchInput } from 'evergreen-ui';
 import { connect } from 'react-redux';
 import { searchActions } from '../state-management/search/actions';
 
 const NavSearch = (props) => {
-    const { dispatch, loading } = props;
+    const { dispatch, loading, results } = props;
+    const [isShown, setIsShown] = useState(false);
 
     const handleKeyPress = (ev) => {
         if (ev.keyCode === 13) {
             console.log(`ev: ${ev.target.value}`);
+            setIsShown(true);
 
             dispatch(searchActions.searchQuery());
         }
@@ -17,6 +19,27 @@ const NavSearch = (props) => {
     return (
         <div>
             <SearchInput placeholder="Search..." onKeyDown={(ev) => handleKeyPress(ev)} />
+
+            <SideSheet
+                    isShown={isShown}
+                    onCloseComplete={() => setIsShown(false)}
+                    containerProps={{
+                    display: 'flex',
+                    flex: '1',
+                    flexDirection: 'column',
+                    }}>
+                    <Pane zIndex={1} flexShrink={0} elevation={0} backgroundColor="white">
+                        <Pane padding={16}>
+                            <Heading size={600}>Search Results</Heading>
+                            <Paragraph size={400}>
+                            # results
+                            </Paragraph>
+                        </Pane>
+                    </Pane>
+                    <Pane flex="1" overflowY="scroll" background="tint1" padding={16}>
+                        {results ? results.map( r => <div>{r.name}</div> ) : ''}
+                    </Pane>
+                </SideSheet>
         </div>
     );
 };
